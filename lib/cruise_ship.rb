cc1823304e45b4853f32bc7a23d7d81838e0a388
Taylor Treece <tayloratreece@gmail.com>
2021-01-05 18:09:56 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,17 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = " "
+  passengers.each do |k, v|
+    if k == :suite_a && v.start_with?("A")
+      winner = v
+      end
+    end
+  winner
 end
